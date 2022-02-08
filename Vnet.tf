b11dfbfa76a2b04f8446db90e6cea57bc07845c7
almayusuf <ysfalma@gmail.com>
2022-02-08 19:07:27 +0000

    Vnet

---
+++ Vnet.tf
@@ -0,0 +1,6 @@
+#VNet
+resource "azurerm_virtual_network" "vnet" {
+  name                = "virtual-network"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  address_space       = ["10.0.0.0/16"]
