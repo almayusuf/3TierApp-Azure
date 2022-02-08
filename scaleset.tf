d874b83fb26083edef4d8a6f44c424fbeea90820
almayusuf <ysfalma@gmail.com>
2022-02-08 22:21:25 +0000

    Fix quate

--- scaleset.tf
+++ scaleset.tf
@@ -39,28 +39,28 @@ resource "azurerm_linux_virtual_machine_scale_set" "linuxss" {
 
 
 
-resource “azurerm_linux_virtual_machine” “vmubuntu” {
-  name                = “vmubuntu”
+resource "azurerm_linux_virtual_machine" "vmubuntu" {
+  name                = "vmubuntu"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
-  size                = “Standard_F2
-  admin_username      = “adminuser”
+  size                = "Standard_F2"
+  admin_username      = "adminuser"
   network_interface_ids = [
     azurerm_network_interface.nic2.id,
   ]
   admin_ssh_key {
-    username   = “adminuser”
-    public_key = file(“~/.ssh/id_rsa.pub”)
+    username   = "adminuser"
+    public_key = file("~/.ssh/id_rsa.pub")
   }
   os_disk {
-    caching              = “ReadWrite”
-    storage_account_type = “Standard_LRS”
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
   }
   source_image_reference {
-    publisher = “Canonical”
-    offer     = “UbuntuServer”
-    sku       = “16.04-LTS”
-    version   = “latest”
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "16.04-LTS"
+    version   = "latest"
   }
 }
 
