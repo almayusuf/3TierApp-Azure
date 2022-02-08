76a48effe263a61e3552a2c136d294f4bfae46ed
almayusuf <ysfalma@gmail.com>
2022-02-08 20:07:38 +0000

    Assigned Vnet

--- Vnet.tf
+++ Vnet.tf
@@ -11,11 +11,13 @@ resource "azurerm_subnet" "subnet1" {
   name                = "subnet1"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefix      = "10.0.1.0/24"
 }
 resource "azurerm_subnet" "subnet2" {
   name                = "subnet2"
   location            = azurerm_resource_group.rg.location
+  virtual_network_name = azurerm_virtual_network.vnet.name
   resource_group_name = azurerm_resource_group.rg.name
   address_prefix      = "10.0.2.0/24"
 }
@@ -23,6 +25,7 @@ resource "azurerm_subnet" "subnet2" {
 resource "azurerm_subnet" "subnet3" {
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
   name                = "subnet3"
   address_prefix      = "10.0.3.0/24"
 }
