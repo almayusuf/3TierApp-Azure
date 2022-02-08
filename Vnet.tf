bfc833c42f9ee41b77afbb36e8623f0e1be93751
almayusuf <ysfalma@gmail.com>
2022-02-08 20:50:29 +0000

    linuxss

--- Vnet.tf
+++ Vnet.tf
@@ -8,21 +8,21 @@ resource "azurerm_virtual_network" "vnet" {
 
 #Subnet
 resource "azurerm_subnet" "subnet1" {
-  name                = "subnet1"
-  resource_group_name = azurerm_resource_group.rg.name
+  name                 = "subnet1"
+  resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefix      = "10.0.1.0/24"
+  address_prefix     = "10.0.1.0/24"
 }
 resource "azurerm_subnet" "subnet2" {
-  name                = "subnet2"
+  name                 = "subnet2"
   virtual_network_name = azurerm_virtual_network.vnet.name
-  resource_group_name = azurerm_resource_group.rg.name
-  address_prefix      = "10.0.2.0/24"
+  resource_group_name  = azurerm_resource_group.rg.name
+  address_prefix     = "10.0.2.0/24"
 }
 
 resource "azurerm_subnet" "subnet3" {
-  resource_group_name = azurerm_resource_group.rg.name
+  resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
-  name                = "subnet3"
-  address_prefix      = "10.0.3.0/24"
+  name                 = "subnet3"
+  address_prefix     = "10.0.3.0/24"
 }
---
+++ scaleset.tf
@@ -0,0 +1,36 @@
+resource "azurerm_linux_virtual_machine_scale_set" "linuxss" {
+  name                = "linuxvmss"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  sku                 = "Standard_F2"
+  instances           = 2
+  admin_username      = "adminuser"
+
+  admin_ssh_key {
+    username   = "adminuser"
+    public_key = file("~/.ssh/id_rsa.pub")
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "16.04-LTS"
+    version   = "latest"
+  }
+
+  os_disk {
+    storage_account_type = "Standard_LRS"
+    caching              = "ReadWrite"
+  }
+
+  network_interface {
+    name    = "nic2"
+    primary = true
+
+    ip_configuration {
+      name      = "public2"
+      primary   = true
+      subnet_id = azurerm_subnet.subnet2.id
+    }
+  }
+}
