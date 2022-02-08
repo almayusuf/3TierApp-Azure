6d9f58d65a14cc4e8bec1d41b4ac07ff9e2c3fb5
almayusuf <ysfalma@gmail.com>
2022-02-08 19:49:18 +0000

    NIC

--- ips.tf
+++ ips.tf
@@ -20,3 +20,29 @@ resource "azurerm_public_ip" "ip3" {
   allocation_method   = "Dynamic"
 }
 
+
+resource "azurerm_network_interface" "nic1" {
+  name                = "nic1"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "public1"
+    subnet_id                     = azurerm_subnet.subnet1.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.ip1.id
+  }
+}
+
+resource "azurerm_network_interface" "nic2" {
+  name                = "nic2"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "public2"
+    subnet_id                     = azurerm_subnet.subnet1.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.ip2.id
+  }
+}
