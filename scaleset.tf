0f8e4359a0fc5e8eaa78c31d66b0c6d28f9f4f2f
almayusuf <ysfalma@gmail.com>
2022-02-09 22:51:44 +0000

    Fixed Vm names

--- scaleset.tf
+++ scaleset.tf
@@ -1,12 +1,12 @@
 resource "azurerm_linux_virtual_machine_scale_set" "linuxss" {
-  name                = "linuxvmss"
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
-  sku                 = "Standard_F2"
-  instances           = 2
-  admin_username      = "adminuser"
+  name                 = "linuxvmss"
+  resource_group_name  = azurerm_resource_group.rg.name
+  location             = azurerm_resource_group.rg.location
+  sku                  = "Standard_F2"
+  instances            = 1
+  admin_username       = "adminuser"
   computer_name_prefix = "linuxssvm"
-  user_data = filebase64("/home/ec2-user/3TierApp-Azure/wordpress.sh")
+  user_data            = filebase64("/home/ec2-user/3TierApp-Azure/wordpress.sh")
 
   admin_ssh_key {
     username   = "adminuser"
@@ -34,32 +34,5 @@ resource "azurerm_linux_virtual_machine_scale_set" "linuxss" {
       primary   = true
       subnet_id = azurerm_subnet.subnet1.id
     }
-  }
-}
-
-
-
-resource "azurerm_linux_virtual_machine" "linuxvm" {
-  name                = "linuxssvm"
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
-  size                = "Standard_F2"
-  admin_username      = "adminuser"
-  network_interface_ids = [
-    azurerm_network_interface.nic1.id,
-  ]
-  admin_ssh_key {
-    username   = "adminuser"
-    public_key = file("~/.ssh/id_rsa.pub")
-  }
-  os_disk {
-    caching              = "ReadWrite"
-    storage_account_type = "Standard_LRS"
-  }
-  source_image_reference {
-    publisher = "Canonical"
-    offer     = "UbuntuServer"
-    sku       = "16.04-LTS"
-    version   = "latest"
   }
 }
